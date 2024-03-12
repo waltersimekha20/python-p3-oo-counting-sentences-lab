1e30e1408695169c77f514377418759f109d1d1a
waltersimekha <waltersimekha@gmail.com>
2024-03-12 09:16:44 +0300

    done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,30 @@
-#!/usr/bin/env python3
+# Save this code in a file named 'count_sentences.py'
 
 class MyString:
-  pass
+    def __init__(self, value=None):
+        self.value = value
+
+    def is_sentence(self):
+        if self.value is None or not isinstance(self.value, str):
+            print("The value must be a string.")
+            return False
+        return self.value.endswith(".")
+
+    def is_question(self):
+        if self.value is None or not isinstance(self.value, str):
+            print("The value must be a string.")
+            return False
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        if self.value is None or not isinstance(self.value, str):
+            print("The value must be a string.")
+            return False
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        if self.value is None or not isinstance(self.value, str):
+            print("The value must be a string.")
+            return 0
+        sentences = filter(None, [s.strip() for s in self.value.split('.') + self.value.split('?') + self.value.split('!')])
+        return len(sentences)
